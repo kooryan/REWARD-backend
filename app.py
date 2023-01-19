13ba0481f10bbc448489b7ae8b4db7fa0b0b3714
Ryan Koo <kooryan03@gmail.com>
2023-01-19 12:33:25 -0600

    backend updates

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ db = get_collection()
 check = 0
 @name_space.route("/activity")
 class MainClass(Resource):
-    
+
     check = 0
     @app.doc(responses={200: 'OK', 400: 'Invalid Argument', 500: 'Mapping Key Error'},
              params={'activity': 'data from most recent writing activity',
@@ -175,8 +175,9 @@ class MainClass(Resource):
             pos += 1
         return pos
 
-    def pasteHandler(self, info, pre, cur, order, lineNum):
+    def pasteHandler(self, info, pre, cur, order):
         charNum = self.pasteCountChar(info["text"], info["revision"])
+        lineNum = info['line']
         i = 0
         list1 = []
         list2 = []
@@ -224,17 +225,138 @@ class MainClass(Resource):
                 for each in list1:
                     end = end + each + ' '
                 info['changes'] = '(' + str(lineNum) + ',' + str(charNum) + ')' + ", " + end + "---added"
-        info.pop("state")
         info["text"] = info.pop("revision")
-        info["paste"] = info.pop("cb")
+        return info
+
+    def typeHandler(self, info):
+        text = info['revision']
+        lineNum = info['line']
+        length = len(text)
+        changes = []
+        swapword = []
+        index = 0
+        for i in range(length):
+            front = ""
+            back = ""
+            if len(text[i]) > 2 or text[i][0] != -1:
+                continue
+            elif (text[i][0] == -1) and (0 < i < length - 1):
+                check2, front = self.findfront(i, 1, text)
+                check1, back = self.findback(i, -1, 1, text)
+                pos = self.countChar(i, text)
+                if check1 or check2:
+                    swapword.append(
+                        '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + text[i][1] + back + "->")
+                elif front + back != "":
+                    check2, front1 = self.findfront(i, -1, text)
+                    check1, back1 = self.findback(i, -1, -1, text)
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + text[i][
+                        1] + back + "->" + front1 + back1)
+                else:
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
+
+            elif (text[i][0] == -1) and i == 0:
+                check1, back = self.findback(i, -1, 1, text)
+                pos = self.countChar(i, text)
+                if front + back != "":
+                    check1, back1 = self.findback(i, -1, -1, text)
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + text[i][
+                        1] + back + "->" + front + back1)
+                else:
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
+
+            elif (text[i][0] == -1) and (i + 1 == length):
+                pos = self.countChar(i, text)
+                if " " in text[i][1] or "\n" in text[i][1]:
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
+                elif length == 1:
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
+                else:
+                    check2, front = self.findfront(i, 1, text)
+                    if front + back != "":
+                        check2, front1 = self.findfront(i, -1, text)
+                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front1 + text[i][
+                            1] + back + "->" + front + back)
+                    else:
+                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
+
+        for i in range(length):
+            front = ""
+            back = ""
+            if len(text[i]) > 2 or text[i][0] != 1:
+                continue
+            elif (text[i][0] == 1) and (0 < i < length - 1):
+                pos = self.countChar(i, text)
+                if i == length - 2 and (text[length - 1][1].isspace() or text[length - 1][1] == "\n"):
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
+                else:
+                    check1, back = self.findback(i, 1, -1, text)
+                    check2, front = self.findfront(i, -1, text)
+                    if (check1 or check2) and (swapword != []):
+                        swapword[index] += (front + text[i][1] + back)
+                        changes.append(swapword[index])
+                        index += 1
+                    elif front + back != "":
+                        check2, front1 = self.findfront(i, 1, text)
+                        check1, back1 = self.findback(i, 1, 1, text)
+                        changes.append(
+                            '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front1 + back1 + "->" + front +
+                            text[i][1] + back)
+                    else:
+                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
+
+            elif (text[i][0] == 1) and (i == 0):
+                check1, back = self.findback(i, 1, -1, text)
+                pos = self.countChar(i, text)
+                if front + back != "":
+                    check1, back1 = self.findback(i, 1, 1, text)
+                    changes.append(
+                        '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + back1 + "->" + front + text[i][
+                            1] + back)
+                else:
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
+            elif (text[i][0] == 1) and (i + 1 == length):
+                check, front = self.findfront(i, -1, text)
+                pos = self.countChar(i, text)
+                if check:
+                    swapword[index] += (front + text[i][1])
+                    changes.append(swapword[index])
+                elif text[i][1][0] == " " or text[i][1][0] == "\n":
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
+                elif length == 1:
+                    changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
+                else:
+                    check2, front = self.findfront(i, -1, text)
+                    if front + back != "":
+                        check1, back1 = self.findback(i, 1, 1, text)
+                        changes.append(
+                            '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + back + "->" + front +
+                            text[i][1] + back1)
+                    else:
+                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
+        return changes
+
+    def copyHandler(self, info):
+        text = info['text']
+        clipboard = info['cb']
+        linenumbers = info['copyLineNumbers']
+        i = text.find(clipboard)
+        charPos = 0
+        linePos = 0
+        for k in range(i, -1, -1):
+            if text[k] == '\n':
+                linePos += 1
+        for l in range(i-1, -1, -1):
+            if text[l] == '\n':
+                break
+            charPos += 1
+        info['cb'] = '(' + str(linenumbers[linePos]) + ',' + str(charPos) + ')' + ", " + info['cb']
         return info
 
     def post(self):
         try:
             info = request.get_json(force=True)
-            # db.activity.insert_one(info)
             state = info['state']
-            lineNum = info['line']
             if state == 3:
                 pre = []
                 for s in info["text"].splitlines():
@@ -242,129 +364,36 @@ class MainClass(Resource):
                 cur = []
                 for s in info["revision"].splitlines():
                     cur.extend(sent_tokenize(s))
-                pre = sent_tokenize(info["text"])
-                cur = sent_tokenize(info["revision"])
+                # pre = sent_tokenize(info["text"])
+                # cur = sent_tokenize(info["revision"])
                 if len(pre) > len(cur):
-                    info = self.pasteHandler(info, cur, pre, 2, lineNum)
+                    info = self.pasteHandler(info, cur, pre, 2)
                 else:
-                    info = self.pasteHandler(info, pre, cur, 1, lineNum)
-                db.activity.insert_one(info)
-                return {
-                    "status": "Updated recent writing actions in doc",
-                }
-            text = info['revision']
-            length = len(text)
-            changes = []
-            swapword = []
-            index = 0
-            for i in range(length):
-                front = ""
-                back = ""
-                if len(text[i]) > 2 or text[i][0] != -1:
-                    continue
-                elif (text[i][0] == -1) and (0 < i < length - 1):
-                    check2, front = self.findfront(i, 1, text)
-                    check1, back = self.findback(i, -1, 1, text)
-                    pos = self.countChar(i, text)
-                    if check1 or check2:
-                        swapword.append(
-                            '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + text[i][1] + back + "->")
-                    elif front + back != "":
-                        check2, front1 = self.findfront(i, -1, text)
-                        check1, back1 = self.findback(i, -1, -1, text)
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + text[i][
-                            1] + back + "->" + front1 + back1)
-                    else:
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
-
-                elif (text[i][0] == -1) and i == 0:
-                    check1, back = self.findback(i, -1, 1, text)
-                    pos = self.countChar(i, text)
-                    if front + back != "":
-                        check1, back1 = self.findback(i, -1, -1, text)
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + text[i][
-                            1] + back + "->" + front + back1)
-                    else:
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
-
-                elif (text[i][0] == -1) and (i + 1 == length):
-                    pos = self.countChar(i, text)
-                    if " " in text[i][1]:
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
-                    elif length == 1:
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
-                    else:
-                        check2, front = self.findfront(i, 1, text)
-                        if front + back != "":
-                            check2, front1 = self.findfront(i, -1, text)
-                            changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front1 + text[i][
-                                1] + back + "->" + front + back)
-                        else:
-                            changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---deleted")
-
-            for i in range(length):
-                front = ""
-                back = ""
-                if len(text[i]) > 2 or text[i][0] != 1:
-                    continue
-                elif (text[i][0] == 1) and (0 < i < length - 1):
-                    pos = self.countChar(i, text)
-                    if i == length - 2 and (text[length - 1][1].isspace() or text[length - 1][1] == "\n"):
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
-                    else:
-                        check1, back = self.findback(i, 1, -1, text)
-                        check2, front = self.findfront(i, -1, text)
-                        if (check1 or check2) and (swapword != []):
-                            swapword[index] += (front + text[i][1] + back)
-                            changes.append(swapword[index])
-                            index += 1
-                        elif front + back != "":
-                            check2, front1 = self.findfront(i, 1, text)
-                            check1, back1 = self.findback(i, 1, 1, text)
-                            changes.append(
-                                '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front1 + back1 + "->" + front +
-                                text[i][1] + back)
-                        else:
-                            changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
-
-                elif (text[i][0] == 1) and (i == 0):
-                    check1, back = self.findback(i, 1, -1, text)
-                    pos = self.countChar(i, text)
-                    if front + back != "":
-                        check1, back1 = self.findback(i, 1, 1, text)
-                        changes.append(
-                            '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + back1 + "->" + front + text[i][
-                                1] + back)
-                    else:
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
-                elif (text[i][0] == 1) and (i + 1 == length):
-                    pos = self.countChar(i, text)
-                    if text[i][1][0] == " " or text[i][1][0] == "\n":
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
-                    elif length == 1:
-                        changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
-                    else:
-                        check2, front = self.findfront(i, -1, text)
-                        if front + back != "":
-                            check1, back1 = self.findback(i, 1, 1, text)
-                            changes.append(
-                                '(' + str(lineNum) + ',' + str(pos) + ')' + ", " + front + back + "->" + front +
-                                text[i][1] + back1)
-                        else:
-                            changes.append('(' + str(lineNum) + ',' + str(pos) + ')' + ", " + text[i][1] + "---added")
-
-            info["changes"] = changes
-            if state == 0:
-              info.pop("state")
-              info.pop("cb")
-            elif state == 1:
-              info.pop("state")
-              info["cut"] = info.pop("cb")
+                    info = self.pasteHandler(info, pre, cur, 1)
             elif state == 2:
-              info.pop('state')
-              info["copy"] = info.pop("cb")
+                info = self.copyHandler(info)
+            else:
+                changes = self.typeHandler(info)
+                info["changes"] = changes
+
+            if state == 0:
+                info.pop('line')
+                info.pop("state")
+                info.pop("cb")
+            elif state == 1:
+                info.pop('line')
+                info.pop("state")
+                info["cut"] = info.pop("cb")
+            elif state == 2:
+                info.pop('state')
+                info.pop('copyLineNumbers')
+                info["copy"] = info.pop("cb")
+            elif state == 3:
+                info.pop('line')
+                info.pop('state')
+                info["paste"] = info.pop("cb")
+            info["ip address"] = request.remote_addr
             db.activity.insert_one(info)
-            # print(info)
 
             return {
                 "status": "Updated recent writing actions in doc",
